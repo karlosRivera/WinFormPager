be16ed1b85a354c733eae634cc451c67d2e54074
tridipkolkata <tridip2000@gmail.com>
2017-03-10 19:21:11 +0530

    developing pager with buttons

---
+++ DataGridView_CS/ucControl/ButtonPager.Designer.cs
@@ -0,0 +1,57 @@
+﻿namespace DataGridView_CS.ucControl
+{
+    partial class ButtonPager
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pnlPager = new System.Windows.Forms.Panel();
+            this.SuspendLayout();
+            // 
+            // pnlPager
+            // 
+            this.pnlPager.Dock = System.Windows.Forms.DockStyle.Top;
+            this.pnlPager.Location = new System.Drawing.Point(0, 0);
+            this.pnlPager.Name = "pnlPager";
+            this.pnlPager.Size = new System.Drawing.Size(343, 26);
+            this.pnlPager.TabIndex = 2;
+            // 
+            // ButtonPager
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.pnlPager);
+            this.Name = "ButtonPager";
+            this.Size = new System.Drawing.Size(343, 26);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel pnlPager;
+    }
+}
---
+++ DataGridView_CS/ucControl/ButtonPager.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace DataGridView_CS.ucControl
+{
+    public partial class ButtonPager : UserControl
+    {
+        int PageSize = 5;
+
+        public event EventHandler<int> PageChanged;
+
+
+        public ButtonPager()
+        {
+            InitializeComponent();
+        }
+
+        public void BindPager(int recordCount, int currentPage)
+        {
+            List<Page> pages = new List<Page>();
+            int startIndex, endIndex;
+            int pagerSpan = 5;
+
+            //Calculate the Start and End Index of pages to be displayed.
+            double dblPageCount = (double)((decimal)recordCount / Convert.ToDecimal(PageSize));
+            int pageCount = (int)Math.Ceiling(dblPageCount);
+            startIndex = currentPage > 1 && currentPage + pagerSpan - 1 < pagerSpan ? currentPage : 1;
+            endIndex = pageCount > pagerSpan ? pagerSpan : pageCount;
+            if (currentPage > pagerSpan % 2)
+            {
+                if (currentPage == 2)
+                {
+                    endIndex = 5;
+                }
+                else
+                {
+                    endIndex = currentPage + 2;
+                }
+            }
+            else
+            {
+                endIndex = (pagerSpan - currentPage) + 1;
+            }
+
+            if (endIndex - (pagerSpan - 1) > startIndex)
+            {
+                startIndex = endIndex - (pagerSpan - 1);
+            }
+
+            if (endIndex > pageCount)
+            {
+                endIndex = pageCount;
+                startIndex = ((endIndex - pagerSpan) + 1) > 0 ? (endIndex - pagerSpan) + 1 : 1;
+            }
+
+            //Add the First Page Button.
+            if (currentPage > 1)
+            {
+                pages.Add(new Page { Text = "First", Value = "1" });
+            }
+
+            //Add the Previous Button.
+            if (currentPage > 1)
+            {
+                pages.Add(new Page { Text = "<<", Value = (currentPage - 1).ToString() });
+            }
+
+            for (int i = startIndex; i <= endIndex; i++)
+            {
+                pages.Add(new Page { Text = i.ToString(), Value = i.ToString(), Selected = i == currentPage });
+            }
+
+            //Add the Next Button.
+            if (currentPage < pageCount)
+            {
+                pages.Add(new Page { Text = ">>", Value = (currentPage + 1).ToString() });
+            }
+
+            //Add the Last Button.
+            if (currentPage != pageCount)
+            {
+                pages.Add(new Page { Text = "Last", Value = pageCount.ToString() });
+            }
+
+            //Clear existing Pager Buttons.
+            pnlPager.Controls.Clear();
+
+            //Loop and add Buttons for Pager.
+            int count = 0;
+            foreach (Page page in pages)
+            {
+                Button btnPage = new Button();
+                btnPage.Location = new System.Drawing.Point(38 * count, 5);
+                btnPage.Size = new System.Drawing.Size(35, 20);
+                btnPage.Name = page.Value;
+                btnPage.Text = page.Text;
+                btnPage.Enabled = !page.Selected;
+                btnPage.Click += new System.EventHandler(this.Page_Click);
+                pnlPager.Controls.Add(btnPage);
+                count++;
+            }
+
+        }
+
+        private void Page_Click(object sender, EventArgs e)
+        {
+            Button btnPager = (sender as Button);
+            if (this.PageChanged != null)
+                this.PageChanged(this, Convert.ToInt32(btnPager.Name));
+        }
+    }
+
+    class Page
+    {
+        public string Text { get; set; }
+        public string Value { get; set; }
+        public bool Selected { get; set; }
+    }
+}
